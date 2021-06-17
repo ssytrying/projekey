ef65e9d1048016d90962ab417135cfbe74935d52
ssyigitarslan <ssytrying@gmail.com>
2021-06-17 12:52:52 +0300

    asd
    asd

---
+++ MatrixKeypadPeriodic_4C123/matrix.d
@@ -0,0 +1,4 @@
+.\matrix.o: matrix.c
+.\matrix.o: FIFO.h
+.\matrix.o: inc/tm4c123gh6pm.h
+.\matrix.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MatrixKeypadPeriodic_4C123/matrixmaintest.d
@@ -0,0 +1,5 @@
+.\matrixmaintest.o: MatrixMainTest.c
+.\matrixmaintest.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\matrixmaintest.o: inc/tm4c123gh6pm.h
+.\matrixmaintest.o: PLL.h
+.\matrixmaintest.o: matrix.h
