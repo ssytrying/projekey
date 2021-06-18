8e8b24428b718a3a3a321236108be7f0dff5c75d
ssyigitarslan <ssytrying@gmail.com>
2021-06-18 10:34:54 +0300

    asd
    asd

--- uart/pll.d
+++
@@ -1,3 +0,0 @@
-.\pll.o: PLL.c
-.\pll.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\pll.o: PLL.h
--- uart/uarttestmain.d
+++
@@ -1,5 +0,0 @@
-.\uarttestmain.o: UARTTestMain.c
-.\uarttestmain.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\uarttestmain.o: PLL.h
-.\uarttestmain.o: UART.h
-.\uarttestmain.o: inc/tm4c123gh6pm.h
